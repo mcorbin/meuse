d2c9c72ddde9ead161d3c5e147a466abf8a81957
mcorbin <corbin.math@gmail.com>
2019-06-27 17:35:21 +0200

    delete token integration tests

--- test/meuse/integration/integration_test.clj
+++ test/meuse/integration/integration_test.clj
@@ -144,83 +144,157 @@
                      :throw-exceptions false}))
       ;; delete user
       (testing "creating user: success"
-            (test-http
-             {:status 200
-              :body (js {:ok true})}
-             (client/post (str meuse-url "/api/v1/meuse/user")
-                          {:headers {"Authorization" token}
-                           :content-type :json
-                           :body (js {:description "integration test user"
-                                      :password "azertyui"
-                                      :name "integration_deleted"
-                                      :active true
-                                      :role "tech"})
-                           :throw-exceptions false})))
+        (test-http
+         {:status 200
+          :body (js {:ok true})}
+         (client/post (str meuse-url "/api/v1/meuse/user")
+                      {:headers {"Authorization" token}
+                       :content-type :json
+                       :body (js {:description "integration test user"
+                                  :password "azertyui"
+                                  :name "integration_deleted"
+                                  :active true
+                                  :role "tech"})
+                       :throw-exceptions false})))
       (testing "deleting user: success"
-            (test-http
-             {:status 200
-              :body (js {:ok true})}
-             (client/delete (str meuse-url "/api/v1/meuse/user/integration_deleted")
-                          {:headers {"Authorization" token}
-                           :content-type :json
-                           :throw-exceptions false})))
+        (test-http
+         {:status 200
+          :body (js {:ok true})}
+         (client/delete (str meuse-url "/api/v1/meuse/user/integration_deleted")
+                        {:headers {"Authorization" token}
+                         :content-type :json
+                         :throw-exceptions false})))
       (testing "deleting user: not admin"
-            (test-http
-             {:status 403
-              :body (js {:errors [{:detail "bad permissions"}]})}
-             (client/delete (str meuse-url "/api/v1/meuse/user/integration_deleted")
-                            {:headers {"Authorization" integration-token}
-                             :content-type :json
-                             :throw-exceptions false})))
+        (test-http
+         {:status 403
+          :body (js {:errors [{:detail "bad permissions"}]})}
+         (client/delete (str meuse-url "/api/v1/meuse/user/integration_deleted")
+                        {:headers {"Authorization" integration-token}
+                         :content-type :json
+                         :throw-exceptions false})))
       (testing "deleting user: invalid token"
-            (test-http
-             {:status 403
-              :body (js {:errors [{:detail "invalid token"}]})}
-             (client/delete (str meuse-url "/api/v1/meuse/user/integration_deleted")
-                            {:headers {"Authorization" (str integration-token "A")}
-                             :content-type :json
-                             :throw-exceptions false})))
+        (test-http
+         {:status 403
+          :body (js {:errors [{:detail "invalid token"}]})}
+         (client/delete (str meuse-url "/api/v1/meuse/user/integration_deleted")
+                        {:headers {"Authorization" (str integration-token "A")}
+                         :content-type :json
+                         :throw-exceptions false})))
       ;; create token
       (testing "creating token: success"
-            (test-http
-             {:status 200}
-             (client/post (str meuse-url "/api/v1/meuse/token/")
-                          {:content-type :json
-                           :throw-exceptions false
-                           :body (js {:name "new token integration"
-                                      :user "integration"
-                                      :password "azertyui"
-                                      :validity 10})})))
+        (test-http
+         {:status 200}
+         (client/post (str meuse-url "/api/v1/meuse/token/")
+                      {:content-type :json
+                       :throw-exceptions false
+                       :body (js {:name "new token integration"
+                                  :user "integration"
+                                  :password "azertyui"
+                                  :validity 10})})))
+      (testing "creating token: token already exists"
+        (test-http
+         {:status 400
+          :body (js {:errors [{:detail "a token named new token integration already exists for user integration"}]})}
+         (client/post (str meuse-url "/api/v1/meuse/token/")
+                      {:content-type :json
+                       :throw-exceptions false
+                       :body (js {:name "new token integration"
+                                  :user "integration"
+                                  :password "azertyui"
+                                  :validity 10})})))
       (testing "creating token: user does not exist"
-            (test-http
-             {:status 400
-              :body (js {:errors [{:detail "the user foofoofoo does not exist"}]})}
-             (client/post (str meuse-url "/api/v1/meuse/token/")
-                          {:content-type :json
-                           :throw-exceptions false
-                           :body (js {:name "new token integration"
-                                      :user "foofoofoo"
-                                      :password "azertyui"
-                                      :validity 10})})))
+        (test-http
+         {:status 400
+          :body (js {:errors [{:detail "the user foofoofoo does not exist"}]})}
+         (client/post (str meuse-url "/api/v1/meuse/token/")
+                      {:content-type :json
+                       :throw-exceptions false
+                       :body (js {:name "new token integration"
+                                  :user "foofoofoo"
+                                  :password "azertyui"
+                                  :validity 10})})))
       (testing "creating token: invalid password"
-            (test-http
-             {:status 403
-              :body (js {:errors [{:detail "invalid password"}]})}
-             (client/post (str meuse-url "/api/v1/meuse/token/")
-                          {:content-type :json
-                           :throw-exceptions false
-                           :body (js {:name "new token integration"
-                                      :user "integration"
-                                      :password "invalidpassword"
-                                      :validity 10})})))
+        (test-http
+         {:status 403
+          :body (js {:errors [{:detail "invalid password"}]})}
+         (client/post (str meuse-url "/api/v1/meuse/token/")
+                      {:content-type :json
+                       :throw-exceptions false
+                       :body (js {:name "new token integration"
+                                  :user "integration"
+                                  :password "invalidpassword"
+                                  :validity 10})})))
       (testing "creating token: user is not active"
-            (test-http
-             {:status 403
-              :body (js {:errors [{:detail "user is not active"}]})}
-             (client/post (str meuse-url "/api/v1/meuse/token/")
-                          {:content-type :json
-                           :throw-exceptions false
-                           :body (js {:name "new token integration"
-                                      :user "integration_not_active"
-                                      :password "azertyui"
-                                      :validity 10})}))))))
+        (test-http
+         {:status 403
+          :body (js {:errors [{:detail "user is not active"}]})}
+         (client/post (str meuse-url "/api/v1/meuse/token/")
+                      {:content-type :json
+                       :throw-exceptions false
+                       :body (js {:name "new token integration"
+                                  :user "integration_not_active"
+                                  :password "azertyui"
+                                  :validity 10})})))
+      ;; delete token
+      (testing "delete token: success"
+        (test-http
+         {:status 200}
+         (client/delete (str meuse-url "/api/v1/meuse/token/")
+                        {:content-type :json
+                         :headers {"Authorization" integration-token}
+                         :throw-exceptions false
+                         :body (js {:name "new token integration"
+                                    :user "integration"})})))
+      (testing "delete token: an admin can delete a token for another account"
+        ;; first, recreate the token
+        (test-http
+         {:status 200}
+         (client/post (str meuse-url "/api/v1/meuse/token/")
+                      {:content-type :json
+                       :throw-exceptions false
+                       :body (js {:name "new token integration"
+                                  :user "integration"
+                                  :password "azertyui"
+                                  :validity 10})}))
+        (test-http
+         {:status 200}
+         (client/delete (str meuse-url "/api/v1/meuse/token/")
+                        {:content-type :json
+                         :headers {"Authorization" token}
+                         :throw-exceptions false
+                         :body (js {:name "new token integration"
+                                    :user "integration"})})))
+      (testing "delete token: unauthorize"
+        (test-http
+         {:status 403
+          :body (js {:errors [{:detail "user integration cannot delete token for integration_not_active"}]})}
+         (client/delete (str meuse-url "/api/v1/meuse/token/")
+                        {:content-type :json
+                         :headers {"Authorization" integration-token}
+                         :throw-exceptions false
+                         :body (js {:name "integration_token_na"
+                                    :user "integration_not_active"
+                                    })})))
+      (testing "delete token: the token does not exist"
+        (test-http
+         {:status 404
+          :body (js {:errors [{:detail "the token notfound does not exist for the user integration_not_active"}]})}
+         (client/delete (str meuse-url "/api/v1/meuse/token/")
+                        {:content-type :json
+                         :headers {"Authorization" token}
+                         :throw-exceptions false
+                         :body (js {:name "notfound"
+                                    :user "integration_not_active"
+                                    })})))
+      (testing "delete token: the user does not exist"
+        (test-http
+         {:status 404
+          :body (js {:errors [{:detail "the user notfound does not exist"}]})}
+         (client/delete (str meuse-url "/api/v1/meuse/token/")
+                        {:content-type :json
+                         :headers {"Authorization" token}
+                         :throw-exceptions false
+                         :body (js {:name "notfound"
+                                    :user "notfound"
+                                    })})))
+      )))
