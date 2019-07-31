fae2375e2b7ada1ff4f7dcc86a25ed53149153f6
mcorbin <corbin.math@gmail.com>
2019-07-31 23:31:42 +0200

    stay with SNAPSHOT in project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject meuse "0.1.0-alpha2"
+(defproject meuse "0.1.0-SNAPSHOT"
   :description "A Rust registry"
   :url "https://github.com/mcorbin/meuse"
   :license {:name "Eclipse Public License 1.0"}
