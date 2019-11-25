a96b63cd3803a0a071a5bb70acaaae0c85cc29e9
mcorbin <corbin.math@gmail.com>
2019-11-25 22:09:51 +0100

    remove dev schema.sql file

--- postgres.sh
+++ postgres.sh
@@ -2,7 +2,6 @@
 
 docker run -p 5432:5432 -e POSTGRES_DB=meuse -e POSTGRES_USER=meuse \
        -e POSTGRES_PASSWORD=meuse \
-       -v $(pwd)/dev/resources/sql/schema.sql:/docker-entrypoint-initdb.d/schema.sql \
        postgres:11.4
 
 # psql -h localhost -d meuse -p 5432 -U meuse
