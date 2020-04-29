a5cf672878cc21739c2669bdd01b59c060683751
mcorbin <corbin.math@gmail.com>
2020-04-29 23:52:03 +0200

    fix server version check

--- src/meuse/semver.clj
+++ src/meuse/semver.clj
@@ -1,36 +1,63 @@
 (ns meuse.semver
   "Functions to work on crate versions"
-  (:require [clojure.string :as string]))
+  (:require [meuse.log :as log]
+            [clojure.string :as string]))
+
+(defn is-pos-int?
+  [value]
+  (let [int-value (Integer/parseInt value)]
+    (when (< int-value 0)
+      (throw (ex-info "Negative int value" {:value value})))))
+
+(defn extract-number
+  [value]
+  (reduce #(if (or (= \- %2)
+                   (= \+ %2))
+             (reduced %1)
+             (str %1 %2))
+          ""
+          value))
 
 (defn valid?
-  "Checks if a version is a valid semver version."
-  [version]
-  (let [splitted (string/split version #"\.")]
-    ;; check if the version numbers are integers
-    (boolean
-     (and (= 3 (count splitted))
-          (try
-            (mapv #(Integer/parseInt %) splitted)
-            (catch Exception e
-              false))))))
+  [value]
+  (try
+    (let [splitted (string/split value #"\." 3)]
+      (when-not (= 3 (count splitted))
+        (throw (ex-info "Invalid semver version" {:value value})))
+      (let [patch(->> (last splitted)
+                      extract-number)]
+        (mapv is-pos-int? [(first splitted) (second splitted) patch])))
+    (catch Exception e
+      (log/error {} e "Invalid semver version")
+      false)))
 
 (defn version->int-vec
   "Takes a semver version, returns a vector of int."
   [version]
-  (->> (string/split version #"\.")
-       (map #(Integer/parseInt %))))
+  (let [splitted (string/split version #"\." 3)
+        patch (extract-number (last splitted))]
+    (map #(Integer/parseInt %) [(first splitted) (second splitted) patch])))
+
+(defn string-number-size
+  [int-vector]
+  (+ 2 (count (reduce str int-vector))))
 
 (defn compare-versions
   "Compares two semver strings."
   [v1 v2]
   (let [v1-vec (version->int-vec v1)
-        v2-vec (version->int-vec v2)]
-    (cond
-      (= v1-vec v2-vec) 0
-      (< (first v1-vec) (first v2-vec)) -1
-      (> (first v1-vec) (first v2-vec)) 1
-      (< (second v1-vec) (second v2-vec)) -1
-      (> (second v1-vec) (second v2-vec)) 1
-      (< (last v1-vec) (last v2-vec)) -1
-      (> (last v1-vec) (last v2-vec)) 1
-      :default 0)))
+        v2-vec (version->int-vec v2)
+        first-comp (cond
+                     (= v1-vec v2-vec) 0
+                     (< (first v1-vec) (first v2-vec)) -1
+                     (> (first v1-vec) (first v2-vec)) 1
+                     (< (second v1-vec) (second v2-vec)) -1
+                     (> (second v1-vec) (second v2-vec)) 1
+                     (< (last v1-vec) (last v2-vec)) -1
+                     (> (last v1-vec) (last v2-vec)) 1
+                     :else 0)]
+    (if (and (zero? first-comp)
+             (not= (count v1) (string-number-size v1-vec)))
+      (.compareTo (subs v1 (string-number-size v1-vec))
+                  (subs v2 (string-number-size v1-vec)))
+      first-comp)))
--- test/meuse/semver_test.clj
+++ test/meuse/semver_test.clj
@@ -6,24 +6,46 @@
   (is (valid? "1.0.1"))
   (is (valid? "1.0.10"))
   (is (valid? "3.4.10"))
+  (is (valid? "7.0.0-alpha.4"))
+  (is (valid? "7.0.0-alpha+110"))
+  (is (valid? "7.0.0-alpha+11acea0"))
+  (is (valid? "7.0.0+10abc"))
   (is (not (valid? "3.4.a")))
   (is (not (valid? "3.4")))
   (is (not (valid? "3.3.4.2")))
   (is (not (valid? "3")))
   (is (not (valid? "baaa"))))
 
+(deftest extract-number-test
+  (is (= "10" (extract-number "10-alpha")))
+  (is (= "10" (extract-number "10+alpha"))))
+
 (deftest version->int-vec-test
-  (is (= [1 0 1] (version->int-vec "1.0.1"))
-      (= [2 3 10] (version->int-vec "2.3.10"))))
+  (is (= [1 0 1] (version->int-vec "1.0.1")))
+  (is (= [2 3 10] (version->int-vec "2.3.10")))
+  (is (= [2 3 10] (version->int-vec "2.3.10-alpha.4"))))
+
+(deftest string-number-size-test
+  (is (= 5 (string-number-size [1 1 2])))
+  (is (= 9 (string-number-size [12 130 20]))))
 
 (deftest compare-versions-test
   (is (= 0 (compare-versions "1.0.1" "1.0.1")))
+  (is (= 0 (compare-versions "1.0.1-alpha.4" "1.0.1-alpha.4")))
+  (is (= -1 (compare-versions "1.0.1-alpha.4" "1.0.1-alpha.5")))
+  (is (= -1 (compare-versions "1.0.1+alpha.40" "1.0.1+alpha.50")))
   (is (= -1 (compare-versions "1.0.1" "2.0.1")))
   (is (= -1 (compare-versions "1.0.1" "1.1.1")))
   (is (= -1 (compare-versions "1.0.1" "1.0.2")))
   (is (= -1 (compare-versions "1.2.2" "1.3.1")))
   (is (= -1 (compare-versions "4.0.10" "4.1.1")))
+  (is (= 1 (compare-versions "2.0.1-alpha+10" "1.0.1-alpha+9")))
+  (is (= 1 (compare-versions "2.0.1-alpha+110" "1.0.1-alpha+9")))
+  (is (= 1 (compare-versions "2.0.1-alpha+999" "1.0.1-alpha+9")))
+  (is (= 1 (compare-versions "2.0.1-alpha+90" "1.0.1-alpha+9")))
+  (is (= 1 (compare-versions "2.0.1+10" "1.0.1+9")))
   (is (= 1 (compare-versions "2.0.1" "1.0.1")))
+  (is (= 1 (compare-versions "1.0.10" "1.0.2")))
   (is (= 1 (compare-versions "1.1.1" "1.0.1")))
   (is (= 1 (compare-versions "1.0.2" "1.0.1")))
   (is (= 1 (compare-versions "1.3.1" "1.2.2")))
