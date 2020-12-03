040d8e3a08d00d4a62b8247a30244674aaba5e99
mcorbin <corbin.math@gmail.com>
2020-12-03 20:00:05 +0100

    Release 1.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject meuse "1.1.1"
+(defproject meuse "1.1.2"
   :description "A free private Rust registry"
   :url "https://github.com/mcorbin/meuse"
   :license {:name "Eclipse Public License 2.0"}
